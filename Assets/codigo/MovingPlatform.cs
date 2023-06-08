55f51ccdd4136211a33e0f79828148b91cb40f27
Anastassiya <anastassiya.murashova1617@gmail.com>
2023-06-08 21:17:43 +0300

    Final

--- Assets/codigo/MovingPlatform.cs
+++ Assets/codigo/MovingPlatform.cs
@@ -3,26 +3,19 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class MovingPlatform : MonoBehaviour {
-    public Transform platform;
-    public Transform startPoint;
-    public Transform endPoint;
-    public float speed = 1.5f;
-    int direction = 1;
+    public float speed;
+    public int startingPoint;
+    public Transform[] points;
 
+    private int pointIndex;
+
+    void Start() {
+        transform.position = points[pointIndex].position;
+    }
     private void Update() {
-        Vector2 target = currentMovementTarget();
-        platform.position = Vector2.Lerp(platform.position, target, speed * Time.deltaTime);
+        if (Vector2.Distance(transform.position, points[pointIndex].position) >= 0.02f) return;
+        if (++pointIndex == points.Length)  pointIndex = 0;
+        transform.position = Vector2.MoveTowards(transform.position, points[pointIndex].position, speed * Time.deltaTime);
     }
 
-    Vector2 currentMovementTarget() {
-        return direction == 1 ? startPoint.position : endPoint.position;
-    }
-
-    private void OnDrawGizmos(){
-        // Update for Debug visualization
-        if(platform != null && startPoint != null && endPoint != null) {
-            Gizmos.DrawLine(platform.transform.position, startPoint.position);
-            Gizmos.DrawLine(platform.transform.position, endPoint.position);
-        }
-    }
 }
